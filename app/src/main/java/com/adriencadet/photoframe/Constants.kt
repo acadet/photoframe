552c74346f2d1486f774152bfc14a0b31c09fd46
Adrien Cadet <acadet@lyft.com>
2021-08-23 17:20:14 -0400

    Make it an object

--- app/src/main/java/com/adriencadet/photoframe/Constants.kt
+++ app/src/main/java/com/adriencadet/photoframe/Constants.kt
@@ -1,12 +1,10 @@
 package com.adriencadet.photoframe
 
-class Constants {
-    companion object {
-        const val DURATION_SEC = 60L
-        const val DURATION_SEC_BEFORE_RESTART = DURATION_SEC
+object Constants {
+    const val DURATION_SEC = 60L
+    const val DURATION_SEC_BEFORE_RESTART = DURATION_SEC
 
-        const val INIT_DURATION_SEC = 3L
+    const val INIT_DURATION_SEC = 3L
 
-        const val MAX_PAUSE_DURATION_SEC = 3 * 60L
-    }
+    const val MAX_PAUSE_DURATION_SEC = 3 * 60L
 }
