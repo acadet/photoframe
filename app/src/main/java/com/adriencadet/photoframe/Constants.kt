432603bb3c4c9cc4976eade0f0794869d0fd426c
Adrien Cadet <acadet@lyft.com>
2021-08-24 08:37:47 -0400

    Remove unused constant

--- app/src/main/java/com/adriencadet/photoframe/Constants.kt
+++ app/src/main/java/com/adriencadet/photoframe/Constants.kt
@@ -3,7 +3,5 @@ package com.adriencadet.photoframe
 object Constants {
     const val PHOTO_DURATION_SEC = 60L
 
-    const val DELAY_BEFORE_START_SEC = 3L
-
     const val PAUSE_TIMEOUT_SEC = 3 * 60L
 }
